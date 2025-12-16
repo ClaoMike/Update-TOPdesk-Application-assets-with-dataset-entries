c3bf3202709b3f6e4da185efa7a669e18c5f90a4
Claudiu Mihai Jechel <jechelclaudiumihai@gmail.com>
2025-12-16 14:19:18 +0100

    Refactor

---
+++ ApplicationsExcel.py
@@ -0,0 +1,37 @@
+import pandas as pd
+
+from ConfigurationManager import ConfigurationManager
+from ApiService import ApiService
+
+class ApplicationsExcel:
+
+    def __init__(self):
+        df = pd.read_excel(
+            "DLF Applications.xlsx",
+            sheet_name="Separated Entries",
+            keep_default_na=False  # otherwise, NA will be nan
+        )
+        df["Region"] = df["Region"].where(df["Region"].notna(), None).ffill()
+
+        self.df = df
+
+    def get_entries_where_name_is(self, name: str):
+        return self.df.loc[self.df["Name"] == name] # match the rows where Name matches the input
+
+    def generate_entries(self, name: str):
+        entries = []
+        rows = self.get_entries_where_name_is(name=name)
+
+        for _, row in rows.iterrows():
+            region = row["Region"]
+            department = row["Department"]
+
+            region_id = ConfigurationManager.regions[region]
+            department_id = ConfigurationManager.departments[department]
+
+            # create the dataset entry asset
+            dataset_entry = ApiService.create_dataset_asset(region=region_id, department=department_id)
+            dataset_entry_id = dataset_entry.get('data').get('unid') # extract its id
+            entries.append(dataset_entry_id)
+
+        return entries
--- main.py
+++ main.py
@@ -1,47 +1,21 @@
-import pandas as pd
-
+### load imports #######################################################################################################
 from ApiService import ApiService
 from ConfigurationManager import ConfigurationManager
+from ApplicationsExcel import ApplicationsExcel
 
+### load entities ######################################################################################################
 config = ConfigurationManager()
 api = ApiService()
+apps = ApplicationsExcel()
 
-
-# Read a specific sheet
-df = pd.read_excel(
-    "DLF Applications.xlsx",
-    sheet_name="Separated Entries",
-    keep_default_na=False # otherwise, NA will be nan
-)
-df["Region"] = df["Region"].where(df["Region"].notna(), None).ffill()
-print(df["Region"].unique())
-
+### retrieve all assets (ids) ##########################################################################################
 application_assets = ApiService.get_assets_by_template_id(id=config._topdesk_application_template_id).get('results')
+
 for asset in application_assets:
-    asset_id = asset['id']
+    asset_id = asset['id'] # extract id
 
-    detailed_asset = ApiService.get_asset(id=asset_id)
-    app_id = detailed_asset.get("data").get('name')
+    detailed_asset = ApiService.get_asset(id=asset_id) # fetch the full asset
+    app_id = detailed_asset.get("data").get('name') # extract the app-id
 
-    entries = []
-    rows = df.loc[df["Name"] == app_id]
-
-    print("Matched rows:", len(rows))
-    print(rows[["Name", "Region", "Department"]].to_string(index=False))
-    print("Raw Region values repr:", [repr(x) for x in rows["Region"].tolist()])
-
-    for _, row in rows.iterrows():
-        region = row["Region"]
-        department = row["Department"]
-
-        print(f"Asset id: {asset_id}")
-        print(f"App id: {app_id}")
-        print(region)
-        region_id = config.regions[region]
-        department_id = config.departments[department]
-
-        dataset_entry = ApiService.create_dataset_asset(region=region_id, department=department_id)
-        dataset_entry_id = dataset_entry.get('data').get('unid')
-        entries.append(dataset_entry_id)
-
-    _ = ApiService.update_asset(asset_id=asset_id, entries=entries)
+    entries = apps.generate_entries(name=app_id) # get the new dataset entries
+    _ = ApiService.update_asset(asset_id=asset_id, entries=entries) # update the assets with the new entries
